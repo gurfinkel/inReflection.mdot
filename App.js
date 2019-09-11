b121d3db4d21a6e963729219ba3c146ebc74a9ff
Dmitry <flatorez@ya.ru>
2019-09-11 22:14:37 +0200

    change react-navigation version

--- App.js
+++ App.js
@@ -4,20 +4,22 @@ import { StackNavigator } from 'react-navigation';
 import { Contact } from './app/views/Contact';
 import { Home } from './app/views/Home';
 
-const MyRoutes = StackNavigation({
-    HomeRT: {
-        screen: Home,
-    },
-    
-    ContactRT: {
-        screen: Contact,
-    },
-}, {
-    initialRoutName: 'HomeRT',
-});
+const MyRoutes = StackNavigator(
+    {
+        HomeRT: {
+            screen: Home,
+        },
+        
+        ContactRT: {
+            screen: Contact,
+        },
+    }, {
+        initialRouteName: 'HomeRT',
+    }
+);
 
 export default function App() {
-  return (
-      <MyRoutes></MyRoutes>
-  );
+    return (
+        <MyRoutes></MyRoutes>
+    );
 }
